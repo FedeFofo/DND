46631dc09606f49cb649dc569c694ee3bddd41bd
FedeFofo <98142069+FedeFofo@users.noreply.github.com>
2026-08-25 11:46:56 -0700

    Initial commit

---
+++ PugRescue/PugSaver.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PugSaver {
+
+	// Moves every dog whose breed is "Pug" in the list to the back of the list
+	public static void rescuePugs(ArrayList<Dog> list) {
+
+		System.out.println(list);
+
+		int count = 0;
+
+		for (Dog dog : list) {
+			if (list.indexOf(dog) >= (list.size() - count)) {
+				break;
+			}
+
+			if (dog.getBreed().equals("Pug")) {
+				Dog temp = dog;
+				int pug = list.indexOf(dog);
+				count++;
+
+				for (int i = list.size() - 1; i > pug; i--) {
+					if (!(list.get(i).getBreed().equals("Pug"))) {
+						list.set(pug, list.get(i));
+						list.set(i, temp);
+					}
+				}
+			}
+		}
+
+		System.out.println("\n\n" + list);
+	}
+}
