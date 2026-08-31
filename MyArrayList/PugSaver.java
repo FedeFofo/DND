a45dd0520d3003fe5b947e3f7583d81341f56c89
FedeFofo <98142069+FedeFofo@users.noreply.github.com>
2026-08-30 22:47:51 -0700

    Copy PugSaver.java to MyArrayList folder

--- MyArrayList/PugSaver.java
+++ MyArrayList/PugSaver.java
@@ -3,10 +3,54 @@ import java.util.Objects;
 
 public class PugSaver {
 
-	//Moves every dog whose breed is "Pug" in the list to the back of the list
-	//All non-pugs must remain in the same relative order they were in originally
-	//and all pugs must also remain in the same relative order they were in originally
+	// Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
-	
+
+		System.out.println("\t // BEFORE METHOD RAN:\n");
+		for (Dog dog : list) {
+			System.out.println(dog.toString() + "\n");
+		}
+
+		ArrayList<Dog> pugs = new ArrayList<>();
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getBreed().equals("Pug")) {
+				pugs.add(list.get(i));
+				list.remove(i);
+				i--;
+			}
+		}
+
+		for (Dog pug : pugs) {
+			list.add(pug);
+		}
+
+		// old idea (doesn't work):
+
+		// int count = 0;
+
+		// for (Dog dog : list) {
+		// if (list.indexOf(dog) >= (list.size() - count)) {
+		// break;
+		// }
+
+		// if (dog.getBreed().equals("Pug")) {
+		// Dog temp = dog;
+		// int pug = list.indexOf(dog);
+		// count++;
+
+		// for (int i = list.size() - 1; i > pug; i--) {
+		// if (!(list.get(i).getBreed().equals("Pug"))) {
+		// list.set(pug, list.get(i));
+		// list.set(i, temp);
+		// }
+		// }
+		// }
+		// }
+
+		System.out.println("\t // AFTER METHOD RAN:\n");
+		for (Dog dog : list) {
+			System.out.println(dog.toString() + "\n");
+		}
 	}
 }
